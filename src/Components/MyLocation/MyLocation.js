db10a67c2374ddaa40752167a461cd0d17465a05
YaAlBagdattig379 <rabiulmiah54@gmail.com>
2023-02-18 20:07:14 +0530

    second commit

---
+++ src/Components/MyLocation/MyLocation.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
+
+const containerStyle = {
+  width: '400px',
+  height: '400px'
+};
+
+const center = {
+  lat: 22.572645,
+  lng:  88.363892
+};
+
+function MyComponent() {
+  return (
+    <LoadScript
+      googleMapsApiKey="YOUR_API_KEY"
+    >
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={100}
+      >
+        { /* Child components, such as markers, info windows, etc. */ }
+        <></>
+      </GoogleMap>
+    </LoadScript>
+  )
+}
+
+export default React.memo(MyComponent)
